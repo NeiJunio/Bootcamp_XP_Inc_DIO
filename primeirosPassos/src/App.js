21f6de159581017cac134340e512b8d639d6aa29
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-09-02 00:44:29 -0300

    Initial commit

---
+++ primeirosPassos/src/App.js
@@ -0,0 +1,22 @@
+// DEFINIDO MANUALMENTE
+
+const App = () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    return (
+        <div className="App">
+            <h1>Hello DIO!</h1>
+            {arr.map((item) =>
+                (
+                    <p>
+                        {item} * 5 = {item * 5}
+                    </p>
+                )
+            )}
+        </div>
+    );
+};
+
+export default App;
+
+
